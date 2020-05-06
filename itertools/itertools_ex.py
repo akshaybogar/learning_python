d20cd01e14fab1aa795bfefb014e7b61a985efd1
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-06 23:40:20 +0530

    show examples of accumulate takewhile and dropwhile from itertools

--- itertools/itertools_ex.py
+++ itertools/itertools_ex.py
@@ -18,3 +18,22 @@ count = itertools.count(100, -5)
 print(next(count))
 print(next(count))
 print(next(count))
+
+# Using accumulate from itertools
+#Accumulator by default adds values to the number that occured before
+values = [1, 2, 3, 10, 4, 2, 1]
+acc = itertools.accumulate(values)
+print(list(acc))
+
+# Other functions such as max can also be passed.
+acc1 = itertools.accumulate(values, max)
+print(list(acc1))
+
+#Using dropwhile and takewhile from itertools
+#creating utility function
+def test(x):
+    return x>4
+
+vals = [11, 5, 7, 12, 4, 6, 2]
+print(list(itertools.takewhile(test, vals)))
+print(list(itertools.dropwhile(test, vals)))
