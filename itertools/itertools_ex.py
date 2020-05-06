3c1291a8613eca2f712b85acc7c7acb0e278ec52
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-06 23:24:54 +0530

    example of itertools.count usage

--- itertools/itertools_ex.py
+++ itertools/itertools_ex.py
@@ -5,9 +5,16 @@ standard library
 
 import itertools
 
-#Useing cycle which is infinite
+#Using cycle which is infinite
 names = ['Kane', 'Virat', 'Steve', 'Root']
 c = itertools.cycle(names)
 print(next(c))
 print(next(c))
 print(next(c))
+
+# Using count from itertools which is also infinite.
+#takes 2 parameters initial value and step size which defaults to 1 if left empty
+count = itertools.count(100, -5)
+print(next(count))
+print(next(count))
+print(next(count))
