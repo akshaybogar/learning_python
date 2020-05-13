25b7f27f497dec9dc81a5616ac7b6685abb70e2b
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-13 22:23:48 +0530

    Added enum class example

---
+++ class_and_object/enum_start.py
@@ -0,0 +1,18 @@
+from enum import Enum, unique
+
+# Adding unique decorator makes sure that enum values are unique
+@unique
+class Fruits(Enum):
+    APPLE = 1
+    MANGO = 2
+    GUAVA = 3
+    # APPLE = 4 Uncommenting this line will throw error since duplicate names
+    # are not allowed
+    # GRAPE = 3 Uncommenting this line throws error since duplicate values
+    # are not allowed because class Fruits is secorated with Fruits
+
+# Print type of Fruit.APPLE
+print(type(Fruits.APPLE))
+
+#Print name and value of Fruits.APPLE
+print(Fruits.APPLE.name, Fruits.APPLE.value)
