6d75d96a55e5526a9c8985cbca000e927fcd1e7c
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-17 17:16:39 +0530

    use jinja templating instead of manually defining templates

--- working_with_flask/enrollment/enrollmentapp/routes.py
+++ working_with_flask/enrollment/enrollmentapp/routes.py
@@ -2,5 +2,19 @@ from enrollmentapp import app
 from flask import render_template
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html', login=True)
+
+@app.route('/courses')
+def courses():
+    return render_template('course.html', login=True)
+
+@app.route('/register')
+def register():
+    return render_template('register.html', login=True)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', login=True)
