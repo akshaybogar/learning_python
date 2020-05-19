8a7a4a9cd924cd4d836095987676ea8203327376
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-19 22:56:39 +0530

    working with GET request

--- working_with_flask/enrollment/enrollmentapp/routes.py
+++ working_with_flask/enrollment/enrollmentapp/routes.py
@@ -1,5 +1,6 @@
 from enrollmentapp import app
 from flask import render_template
+from flask import request
 
 @app.route('/')
 @app.route('/index')
@@ -7,11 +8,12 @@ from flask import render_template
 def index():
     return render_template('index.html', index=True)
 
-@app.route('/courses')
-def courses():
+@app.route('/courses/')
+@app.route('/courses/<term>')
+def courses(term="Spring 2019"):
     course_data = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"},{"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
-    return render_template('courses.html', course_data=course_data, courses=True)
+    return render_template('courses.html', course_data=course_data, courses=True, term=term)
 
 @app.route('/register')
 def register():
@@ -20,3 +22,10 @@ def register():
 @app.route('/login')
 def login():
     return render_template('login.html', login=True)
+
+@app.route('/enrollment')
+def enrollment():
+    id = request.args.get('courseID')
+    title = request.args.get('title')
+    term = request.args.get('term')
+    return render_template('enrollment.html', data={"id":id, "title":title, "term":term})
