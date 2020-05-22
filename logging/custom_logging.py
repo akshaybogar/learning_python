71c59d8ed637b6a0713e84dee344380646ea3a8a
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-22 21:57:56 +0530

    make changes only to custom loggers

--- logging/custom_logging.py
+++ logging/custom_logging.py
@@ -6,6 +6,18 @@ extra_data = {"user": "akshay.bogar@abc.com"}
 def main():
     fmtstr = 'User:%(user)s: %(asctime)s: %(levelname)s: %(funcName)s: Line:%(lineno)d \
     %(message)s'
+    '''
+     Below statement will change setting for all loggers which may lead to errors.
+     To change settings only for custom loggers we may use FileHandlers and Streamhandlers
+     logger = logging.getLogger('Custom logging')
+     handler = logging.StreamHandler()
+     handler.setFormatter(fmtstr)
+     file_handler = logging.FileHandler(filename='output.log')
+     file_handler.setFormatter(fmtstr)
+     logger.addHandler(handler)
+     logger.addHandler(file_handler)
+     return logger
+    '''
     logging.basicConfig(level=logging.DEBUG, filename='output.txt',
     filemode='w', format=fmtstr)
     logger = logging.getLogger('Custom logging')
